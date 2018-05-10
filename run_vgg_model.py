a57f08bd96185b1ac75917ece9cb3b8751c6a1cb
cvo9 <cvo9@stanford.edu>
2018-05-10 02:06:50 -0700

    initial implementation of VGG-inspired model

---
+++ run_vgg_model.py
@@ -0,0 +1,75 @@
+# CS231N Final Project
+# Henry Lin (henryln1@stanford.edu) and Christopher Vo (cvo9@stanford.edu)
+# Program to load data, initialize, train, and predict on VGG-inspired model
+
+
+# Imported needed libraries and classes
+import vgg_model as vgg
+
+
+# Define various parameters
+data_path = ''
+num_classes = 10
+batch_size = 32
+num_epochs = 10
+output_path = None
+
+
+# Function to write predictions and accuracy to output files
+# Inputs:
+#	predictions: predictions on test set
+#	accuracy: test accuracy
+#	output_path: name of output directory, if None, defaults to current dir
+# Returns:
+#	pred_output, acc_output: output files of predictions and accuracy to specified directory
+def createOutputFiles(predictions, accuracy, output_path):
+
+	# Modify output path accordingly
+	if output_path != None:
+		output_path += '/'
+	else:
+		output_path = ''
+
+	# Create output files
+	pred_output_name = "test_predictions.txt"
+	acc_output_name = "test_accuracy.txt"
+
+	# Predictions output file
+	pred_output = open(output_path + pred_output_name, "w")
+	pred_output.write("Test Set Predictions\n")
+
+	for pred in predictions:
+		pred_output.write(str(pred) + "\n")
+
+	pred_output.close()
+
+	# Accuracy Output File
+	acc_output = open(output_path + acc_output_name, "w")
+	acc_output.write("Test Set Accuracy: " + str(accuracy) + "\n")
+	acc_output.close()
+
+
+# Main function
+def main():
+
+	# Load data
+	x_train, y_train, x_val, y_val, x_test, y_test = vgg.load_data(data_path)
+	N, FR, W, H, D = x_train.shape
+	input_shape = (FR, W, H, D)
+
+	# Initialize VGG model
+	vgg_model = vgg.vgg_model_init(input_shape, num_classes)
+
+	# Train model
+	vgg_model, history = vgg.train(vgg_model, x_train, y_train, x_val=x_val, y_val=y_val, batch_size=batch_size, num_epochs=num_epochs)
+
+	# Predict on test set and record accuracy
+	test_predictions, test_acc = vgg.predict(vgg_model, x_test, y_test)
+
+	# Write predictions and accuracy to output files
+	createOutputFiles(predictions, accuracy, output_path)
+
+
+# Allows for script to be run as program
+if __name__=="__main__":
+	main()
---
+++ vgg_model.py
@@ -0,0 +1,129 @@
+# CS231N Final Project
+# Henry Lin (henryln1@stanford.edu) and Christopher Vo (cvo9@stanford.edu)
+# Implementation of initial VGGNet-inspired model (scaled down)
+
+
+# Import needed libraries and functions
+import tensorflow as tf
+import numpy as np
+
+
+# TODO: load functions that read and process input data
+# Inputs: 
+#	path: name of path to data
+# Returns:
+#	x_train, y_train, x_val, y_val, x_test, y_test
+# Image frame sets have shape (FR, W, H, D) where FR = # frames per image set, also known as depth in TF (10), W = width, H = height, D = color dimensions, also known as channels in TF (3)
+# Number of classes: C = 10
+def load_data(path):
+	pass
+	return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+# Function that initializes the VGGNet-inspired model
+# Inputs:
+#	input_shape: shape of each input image set, (Fr, W, H, D) in this case
+# Returns:
+#	vgg_model: compiled model ready to be trained
+# Architecture:
+#	Conv layers: 3x3 filter size, stride 1, padding = 'same', with ReLU
+#	Pool layers: 2x2 max pool, stride 2
+#	Structure: 2 Conv (16 filters), Pool, 2 Conv (32 filters), Pool, 3 Conv (64 filters), Pool, FC
+#	TODO: Should incoporate Batch Norm + Dropout
+#	Loss: softmax
+def vgg_model_init(input_shape, num_classes):
+
+	# Define hyperparams, weight initializer, activation, regularization, loss function, and optimizer
+	FR, W, H, D = input_shape
+
+	num_filters = [16, 32, 64] # number of filters in each set of conv layers
+	filter_size = 3 # 3x3 filters
+	filter_stride = 1
+
+	pool_size = 2 # 2x2 max pool
+	pool_stride = 2
+
+    initializer = tf.variance_scaling_initializer(scale=2.0) # initializer for weights
+    activation = tf.nn.relu # ReLU for each Conv layer
+
+    reg_strength = 0.001
+    regularization = tf.contrib.layers.l2_regularizer(reg_strength) # L2 regularization for FC layer
+
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits # NOTE: not sure if this loss function works with the Keras compile/fit model methods, may have to manually implement train method like in homework
+
+    optimizer = tf.train.AdamOptimizer() # optimizer used is Adam
+
+
+    # Define architecture as sequential layers
+    layers = [
+
+    	# Conv Layer Set 1: 2 Conv layers (16 filters), 1 Pool layer
+    	tf.layers.Conv3D(input_shape=input_shape, filters=num_filters[0], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.Conv3D(filters=num_filters[0], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.MaxPooling2D(pool_size=pool_size, strides=pool_stride, padding='valid'),
+
+    	# Conv Layer Set 2: 2 Conv layers (32 filters), 1 Pool layer
+    	tf.layers.Conv3D(filters=num_filters[1], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.Conv3D(filters=num_filters[1], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.MaxPooling2D(pool_size=pool_size, strides=pool_stride, padding='valid'),
+
+    	# Conv Layer Set 3: 3 Conv layers (64 filters), 1 Pool layer
+    	tf.layers.Conv3D(filters=num_filters[2], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.Conv3D(filters=num_filters[2], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.Conv3D(filters=num_filters[2], kernel_size=[FR, filter_size, filter_size], strides=filter_stride, padding='same', activation=activation, kernel_initializer=initializer),
+    	tf.layers.MaxPooling2D(pool_size=pool_size, strides=pool_stride, padding='valid'),
+
+    	# FC Layer
+        tf.layers.Flatten(),
+        tf.layers.Dense(num_classes, kernel_initializer=initializer, kernel_regularizer=regularization)
+    ]
+
+
+    # Initialize model and compile
+	vgg_model = tf.keras.Sequential(layers)
+	vgg_model.compile(loss=loss, optimizer=optimizer)
+
+	return vgg_model
+
+
+# Function to train given compiled model on training data
+# Inputs:
+#	model: compiled model from function such as vgg_model_init
+#	x_train, y_train, x_val (optional), y_val (optional)
+#	batch_size (optional): if None, model.fit defaults to 32
+#	num_epochs: number of epochs
+# Returns:
+#	model: trained model, ready to predict with
+#	history: history object from model.fit method
+def train(model, x_train, y_train, x_val=None, y_val=None, batch_size=None, num_epochs):
+
+	# Define validation data
+	val_data = None
+
+	if x_val != None and y_val != None:
+		val_data = (x_val, y_val)
+
+	# Use model.fit method to train model
+	history = model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=val_data) # NOTE: not entirely sure validation_data param can be set to None
+
+	return model, history
+
+
+# Function to use given model to predict on given data and return metrics (accuracy for now) on predictions
+# Inputs:
+#	model: trained model from function such as train
+#	x, y (optional)
+# Returns:
+#	predictions: predicted classes for input x
+#	accuracy: accuracy of predicted classes given true classes y
+def predict(model, x, y=None):
+
+	# Use model to make predictions
+	predictions = model.predict(x)
+
+	# Get accuracy
+	accuracy = None
+	if y != None:
+		accuracy = np.mean(predictions == y)
+
+	return predictions, accuracy
