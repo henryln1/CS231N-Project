d1d1e69fdd8d6431f7b3d544e6e094d19b68df41
henryln1 <henritienne@gmail.com>
2018-05-09 19:13:09 -0700

    wrote some preprocessing code

---
+++ data_batch.py
@@ -0,0 +1,101 @@
+import numpy as np
+import random
+
+from load_data import *
+
+"""
+
+This file should contain the data class.
+The data class will have all the data stored which we can then extract.
+
+"""
+
+
+
+
+class Data(object):
+
+	def __init__(self, paths_to_images_dict, batch_type = 5):
+		"""
+		Inputs: 
+		paths_to_images_dict is a dictionary of image path to image array stored as 3-D np array
+		batch_type tells it what kind of batch we want:
+			5 means 5-D tensor of Batch x Image Set x Height x Width x RGB
+			4 means 4-D tensor of Batch x Image x Height x Width x RGB
+
+		"""
+
+		self.path_image_dict = paths_to_images_dict
+		self.batch_type = batch_type
+
+		#create dict of path name to label
+
+		self.path_label_dict = {}
+
+		#create a dict of group (video identifier) to label
+		self.video_label_dict = {}
+
+		for path in self.path_image_dict:
+			label = find_label(path)
+			self.path_label_dict[path] = label
+
+			#NOTE: I think here looking at the first 6 characters is enough
+			self.video_label_dict[path[:6]] = label
+
+		#maximum number of frames a single video may have
+		self.max_frame_count = 300
+
+
+
+	def get_random_image():
+		random_path = random.choice(list(self.path_image_dict))
+		array = self.path_image_dict[random_path]
+		label = self.path_label_dict[random_path]
+		return array, label
+
+	def get_random_image_set(image_set_size):
+
+		random_video = random.choice(list(self.video_label_dict))
+		label = self.video_label_dict[random_video]
+
+		relevant_frames = [x for x in self.path_image_dict if random_video in x] #extract all the keys for the chosen video
+		relevant_frames.sort()
+		random_start = random.randInt(0, self.max_frame_count - image_set_size) #do I need to subtract 1?
+
+		relevant_frames = relevant_frames[random_start: random_start + image_set_size]
+
+		frames = []
+		for frame_path in relevant_frames:
+			frames.append(self.path_image_dict[frame_path])
+
+		array = np.stack(frames, axis = 0)
+		#array should be a 4-D tensor
+		return array, label
+
+	def create_batch(batch_size):
+		if self.batch_type == 4:
+			images = []
+			labels = []
+			for i in range(batch_size):
+				image, label = get_random_image
+				images.append(image)
+				labels.append(label)
+			batch = np.stack(images, axis = 0)
+			batch_labels = np.asarray(labels)
+
+		elif self.batch_type == 5:
+			batch = None
+			batch_labels = None
+
+		#batch should be 4 of 5-D tensor, batch_labels should be 1-D, batch_size X 1
+		return batch, batch_labels	
+
+
+
+
+
+
+
+
+
+
---
+++ load_data.py
@@ -0,0 +1,62 @@
+import scipy
+import numpy as np 
+
+
+#global variables
+dance_data_directory_name = 'dance_data'
+directory_path = '../' + dance_data_directory_name + '/'
+classes = ['ballet', 'break', 'flamenco', 'foxtrot', 'latin', 'quickstep', 'square', 'swing', 'tango', 'waltz']
+
+
+
+
+#read in the txt file that contains path to each image
+
+def read_text_file(file_name): #returns a list of paths to the images
+	#file name is the name of text file with all the paths
+	list_paths = []
+	file_path = directory_path + file_name
+
+	with open(file_path, 'r') as f:
+		for line in f.readlines():
+			list_paths.append(directory_path + line[:-1])
+
+	return list_paths
+
+
+from scipy.ndimage import imread
+
+def read_image_file(file_path):
+	#takes in the path to a image file and returns that image file as a 3-D numpy array (H, W, RGB)
+
+	image_array = imread(file_path, flatten = False)
+	return image_array
+
+
+def find_label(file_path): 
+	"""
+	returns an int form of the label, ballet = 0, break = 1, etc.
+	"""
+	for klass_index in range(len(classes)):
+		klass = classes[klass_index]
+		if klass in file_path:
+			return klass_index
+	print("No Class Found, please look at file path")
+	return
+
+def read_all_images(dataset = "train"): #dataset should be train, validation, or test
+	#takes in the dataset name and returns a dictionary of image path name to image as a np array
+	text_file_path = directory_path + dataset + 'txt'
+	image_paths = read_text_file(text_file_path)
+
+	path_image_dict = {}
+	for image_path in image_paths:
+		image_array = read_image_file(image_path)
+		path_image_dict[image_path] = image_array
+
+	return path_image_dict
+
+
+
+
+
